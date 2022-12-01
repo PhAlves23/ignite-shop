202d2d1372ea205e0d28f0656fb0762e1ebd68e9
Paulo Henrique Andrade Alves <80326186+PhAlves23@users.noreply.github.com>
2022-12-01 15:52:36 -0300

    :sparkles: :lipstick: desenvolvendo header e carroussel de produtos

---
+++ src/pages/index.tsx
@@ -0,0 +1,69 @@
+import Image from "next/image";
+
+import { useKeenSlider } from "keen-slider/react";
+import { styled } from "../styles";
+
+import { stripe } from "../lib/stripe";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import camiseta1 from "../assets/camisetas/1.png";
+import camiseta2 from "../assets/camisetas/2.png";
+import camiseta3 from "../assets/camisetas/3.png";
+
+import "keen-slider/keen-slider.min.css";
+import { GetServerSideProps } from "next";
+
+export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+  return (
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={camiseta1} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta2} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await stripe.products.list();
+
+  console.log(response);
+
+  return {
+    props: {
+      list: [1, 2, 3],
+    },
+  };
+};
